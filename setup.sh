98b15ccf3f7d6a7256dbdb6e4565569bf50d08e3
skreutzer <stephankreutzer@gmx.net>
2020-02-18 22:36:49 +0100

    Carry over improvements from the other generation workflow (retaining HTML from the Markdown source for .tex).

--- setup.sh
+++ setup.sh
@@ -6,7 +6,8 @@ unzip ./master.zip
 mv ./Peeragogy.github.io-master/ ./Peeragogy.github.io/
 
 cd ./Peeragogy.github.io/
-grep -o "<a href=\"\./[^\"]*" index.html | sed -r "s/<a href=\"\.\/(.*).html/\1/" | xargs -I {} pandoc -o {}.tex {}.md | pandoc -o workbook.tex workbook.md
+grep -o "<a href=\"\./[^\"]*" index.html | sed -r "s/<a href=\"\.\/(.*).html/\1/" | xargs -I {} pandoc -o {}.html {}.md
+grep -o "<a href=\"\./[^\"]*" index.html | sed -r "s/<a href=\"\.\/(.*).html/\1/" | xargs -I {} pandoc -o {}.tex {}.html
 cp ./*.tex ../en/
 cp -r ./images/ ../en/
 cd ..
